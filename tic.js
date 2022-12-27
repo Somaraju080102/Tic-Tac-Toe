004ce915cac7be06e87c477ec54558bf91cf28ad
Somaraju Indukuri <106017466+Somaraju080102@users.noreply.github.com>
2022-12-27 10:41:45 +0530

    Update tic.js

--- tic.js
+++ tic.js
@@ -1,5 +1,6 @@
 
-       const f1=document.getElementById("1");
+
+    const f1=document.getElementById("1");
        const f2=document.getElementById("2");
        const f3=document.getElementById("3");
        const f4=document.getElementById("4");
@@ -29,10 +30,12 @@
        }
         
        f1.addEventListener("click",function(){
-           if(count<9){
+           if(count<9&&arr[0]!=1&&arr[0]!=2){
            if(count%2==0){
 
-           document.getElementById("1").style.backgroundColor="blue";
+           document.getElementById("1").innerHTML="X";
+           document.getElementById("1").style.color="blue";
+
            arr[0]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -43,7 +46,8 @@
            }
            }
            else{
-               document.getElementById("1").style.backgroundColor="yellow";
+               document.getElementById("1").innerHTML="O";
+               document.getElementById("1").style.color="yellow";
                arr[0]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
@@ -56,13 +60,18 @@
            }
 
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
            
        })
        f2.addEventListener("click",function(){
-           if(count<9){
+           if(count<9&&arr[1]!=1&&arr[1]!=2){
            if(count%2==0){
-           document.getElementById("2").style.backgroundColor="blue";
+            document.getElementById("2").innerHTML="X";
+           document.getElementById("2").style.color="blue";
+
            arr[1]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -76,8 +85,9 @@
            
            }
            else{
-               document.getElementById("2").style.backgroundColor="yellow";
-               arr[1]=2;
+            document.getElementById("2").innerHTML="O";
+               document.getElementById("2").style.color="yellow";
+                              arr[1]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -88,12 +98,17 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f3.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("3").style.backgroundColor="blue";
+        if(count<9&&arr[2]!=1&&arr[2]!=2){
+            if(count%2==0){
+                document.getElementById("3").innerHTML="X";
+           document.getElementById("3").style.color="blue";
+
            arr[2]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -105,8 +120,9 @@
            }
            }
            else{
-               document.getElementById("3").style.backgroundColor="yellow";
-               arr[2]=2;
+            document.getElementById("3").innerHTML="O";
+               document.getElementById("3").style.color="yellow";       
+                       arr[2]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -117,12 +133,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f4.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("4").style.backgroundColor="blue";
+        if(count<9&&arr[3]!=1&&arr[3]!=2){
+            if(count%2==0){
+                document.getElementById("4").innerHTML="X";
+           document.getElementById("4").style.color="blue";
            arr[3]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -134,8 +154,9 @@
            }
            }
            else{
-               document.getElementById("4").style.backgroundColor="yellow";
-               arr[3]=2;
+            document.getElementById("4").innerHTML="O";
+               document.getElementById("4").style.color="yellow";
+                              arr[3]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -146,12 +167,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f5.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("5").style.backgroundColor="blue";
+        if(count<9&&arr[4]!=1&&arr[4]!=2){
+            if(count%2==0){
+                document.getElementById("5").innerHTML="X";
+           document.getElementById("5").style.color="blue";
            arr[4]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -163,8 +188,9 @@
            }
            }
            else{
-               document.getElementById("5").style.backgroundColor="yellow";
-               arr[4]=2;
+            document.getElementById("5").innerHTML="O";
+               document.getElementById("5").style.color="yellow";  
+                            arr[4]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -175,12 +201,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f6.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("6").style.backgroundColor="blue";
+        if(count<9&&arr[5]!=1&&arr[5]!=2){
+            if(count%2==0){
+                document.getElementById("6").innerHTML="X";
+           document.getElementById("6").style.color="blue";
            arr[5]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -192,8 +222,9 @@
            }
            }
            else{
-               document.getElementById("6").style.backgroundColor="yellow";
-               arr[5]=2;
+            document.getElementById("6").innerHTML="O";
+               document.getElementById("6").style.color="yellow";
+                              arr[5]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -203,12 +234,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f7.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("7").style.backgroundColor="blue";
+        if(count<9&&arr[6]!=1&&arr[6]!=2){
+            if(count%2==0){
+                document.getElementById("7").innerHTML="X";
+           document.getElementById("7").style.color="blue";
            arr[6]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -220,8 +255,9 @@
            }
            }
            else{
-               document.getElementById("7").style.backgroundColor="yellow";
-               arr[6]=2;
+            document.getElementById("7").innerHTML="O";
+               document.getElementById("7").style.color="yellow";
+                              arr[6]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -232,12 +268,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f8.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("8").style.backgroundColor="blue";
+        if(count<9&&arr[7]!=1&&arr[7]!=2){
+            if(count%2==0){
+                document.getElementById("8").innerHTML="X";
+           document.getElementById("8").style.color="blue";
            arr[7]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -249,8 +289,9 @@
            }
            }
            else{
-               document.getElementById("8").style.backgroundColor="yellow";
-               arr[7]=2;
+            document.getElementById("8").innerHTML="O";
+               document.getElementById("8").style.color="yellow";
+                              arr[7]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -260,12 +301,16 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
        }
                })
        f9.addEventListener("click",function(){
-           if(count<9){
-           if(count%2==0){
-           document.getElementById("9").style.backgroundColor="blue";
+        if(count<9&&arr[8]!=1&&arr[8]!=2){
+            if(count%2==0){
+                document.getElementById("9").innerHTML="X";
+           document.getElementById("9").style.color="blue";
            arr[8]=1;
            if(arr[0]==1&&arr[3]==1&&arr[6]==1||arr[0]==1&&arr[1]==1&&arr[2]==1||arr[0]==1&&arr[4]==1&&arr[8]==1
            ||arr[2]==1&&arr[4]==1&&arr[6]==1||arr[2]==1&&arr[5]==1&&arr[8]==1||arr[3]==1&&arr[4]==1&&arr[5]==1||
@@ -277,8 +322,9 @@
            }
            }
            else{
-               document.getElementById("9").style.backgroundColor="yellow";
-               arr[8]=2;
+            document.getElementById("9").innerHTML="O";
+               document.getElementById("9").style.color="yellow";
+                              arr[8]=2;
                if(arr[0]==2&&arr[3]==2&&arr[6]==2||arr[0]==2&&arr[1]==2&&arr[2]==2||arr[0]==2&&arr[4]==2&&arr[8]==2
            ||arr[2]==2&&arr[4]==2&&arr[6]==2||arr[2]==2&&arr[5]==2&&arr[8]==2||arr[3]==2&&arr[4]==2&&arr[5]==2||
            arr[6]==2&&arr[7]==2&&arr[8]==2||arr[1]==2&&arr[4]==2&&arr[7]==2){
@@ -289,7 +335,12 @@
            }
            }
            count+=1;
+           if(count==9){
+            document.getElementById("winner").innerHTML="Game Was Draw";
+           }
+
        }
                })
               
      
+
